f8324cdabbc4079b5fa522ba35fdb0b93c7b4cc1
Ben Booth <benwbooth@gmail.com>
2014-12-01 12:13:38 -0800

    add error message if no script was passed in

--- test.pyx
+++ test.pyx
@@ -1,19 +1,20 @@
 #!/usr/bin/env cyrun3
 # #distutils: name = test
 # #distutils: sources = 
-# distutils: define_macros = TESTMACRO
-# distutils: undef_macros = TESTMACRO
+# #distutils: define_macros = TESTMACRO
+# #distutils: undef_macros = TESTMACRO
 # #distutils: libraries = 
-# distutils: library_dirs = test
-# distutils: runtime_library_dirs = test
-# distutils: include_dirs = test
+# #distutils: library_dirs = test
+# #distutils: runtime_library_dirs = test
+# #distutils: include_dirs = test
 # #distutils: extra_objects = 
-# distutils: extra_compile_args = -Wall
+# #distutils: extra_compile_args = -Wall
 # #distutils: extra_link_args = 
 # #distutils: export_symbols = 
 # #distutils: depends = 
-# distutils: language = c++
+# #distutils: language = c++
 import sys
 cdef int i=0
+
 i += 1
-print("Hello World!")
+print("Hello World! "+str(sys.argv))
